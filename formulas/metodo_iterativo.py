b231407f6d3b567233ce05a0fd1a7d236f0e2ed8
Luiyi <luiyiantonyqp@gmail.com>
2024-07-13 18:00:22 +0000

    corrigiendo un error de nombre de variable en el archivo main linea 19

--- formulas/metodo_iterativo.py
+++ formulas/metodo_iterativo.py
@@ -18,12 +18,13 @@ def MetodoIterativo(ancho, peralte, recubrimiento):
     """
     error = 2
     a_tanteado = random.uniform(0.0, peralte)
+    peralte_efectivo = peralte - recubrimiento
     while error > 1:
-        peralte_efectivo = peralte - recubrimiento
         As = momento_ultimo/(res_acero * (peralte_efectivo - (a_tanteado/2)))
         a_calculado = (As * res_acero)/(0.85 * res_concreto * ancho)
         #AHORA CALCULAREMOS EL PORCENTAJE DE ERROR
         error = abs(((a_tanteado - a_calculado)/ a_tanteado ) * 100)
+        print(f"error: {error}")
         if error > 1:
             print("se esta volviendo a tentear")
             a_tanteado = a_calculado
